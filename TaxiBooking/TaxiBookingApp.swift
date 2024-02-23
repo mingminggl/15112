3073acf5f6624b17f1a00c33962cc71e7bfd996a
mingminggl <cglankwa@andrew.cmu.edu>
2024-02-23 00:15:21 -0500

    Initial Commit

---
+++ TaxiBooking/TaxiBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxiBookingApp.swift
+//  TaxiBooking
+//
+//  Created by Chayut Glankwamdee on 23/2/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct TaxiBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
